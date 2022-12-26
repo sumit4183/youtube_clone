2d981770d7561c851176281e4ed4129702354e0b
SUMIT <sumit4183@yahoo.com>
2022-12-26 11:54:44 +0530

    Feed Layout

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
 const options = {
-    params: {maxResult: '50'},
+    params: {maxResults: '50'},
     headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
